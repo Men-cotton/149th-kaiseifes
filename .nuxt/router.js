5df794c023451b3b7ae4af564742b3fdf479a05a
Mencotton <mencotton0410@gmail.com>
2020-10-28 11:47:16 +0900

    Add page for testing fonts

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,8 +3,9 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _405439b6 = () => interopDefault(import('..\\pages\\example.vue' /* webpackChunkName: "pages/example" */))
-const _35377a1e = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _7deb79d7 = () => interopDefault(import('../pages/color.vue' /* webpackChunkName: "pages/color" */))
+const _4190195e = () => interopDefault(import('../pages/example.vue' /* webpackChunkName: "pages/example" */))
+const _5091b874 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -23,12 +24,16 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/color",
+    component: _7deb79d7,
+    name: "color"
+  }, {
     path: "/example",
-    component: _405439b6,
+    component: _4190195e,
     name: "example"
   }, {
     path: "/",
-    component: _35377a1e,
+    component: _5091b874,
     name: "index"
   }],
 
---
+++ pages/color.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hoge">
+    <div class="container-didot">
+      <div class="title">Didot</div>
+      Twilight
+    </div>
+    <div class="container-helvetica">
+      <div class="title">Helvetica</div>
+      Twilight
+    </div>
+    <div class="container-din">
+      <div class="title">DIN</div>
+      Twilight
+    </div>
+    <div class="container-udnewgothic">
+      <div class="title">UD新ゴ</div>
+      Twilight
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'World',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$title-size: 100px;
+
+div[class^='container'] {
+  font-style: normal;
+  font-weight: normal;
+  color: #082449;
+  font-size: 30px;
+  .title {
+    font-size: $title-size;
+  }
+}
+
+.hoge {
+  padding-left: 15px;
+  background-color: #f2743f;
+}
+
+.container-didot {
+  font-family: linotype-didot, serif;
+}
+
+.container-helvetica {
+  font-family: neue-haas-grotesk-display, sans-serif;
+}
+
+.container-din {
+  font-family: din-2014, sans-serif;
+}
+
+.container-udnewgothic {
+  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+}
+</style>
